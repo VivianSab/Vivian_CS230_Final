634b990229678e1e13e96b6b64aa3beb905aa893
VivianSab <132399121+VivianSab@users.noreply.github.com>
2023-05-02 16:33:41 -0700

    Update CS230_MeteoriteLandings_Final_VivianSabga.py

--- CS230_MeteoriteLandings_Final_VivianSabga.py
+++ CS230_MeteoriteLandings_Final_VivianSabga.py
@@ -19,8 +19,8 @@ import pandas as pd
  #   data = pd.read_excel(file_path, engine='openpyxl')
  #   return data
 
-file_path = "Meteroite_Landings.xlsx"
-data = pd.read_excel(file_path)
+#file_path = "Meteroite_Landings.xlsx"
+#data = pd.read_excel(file_path)
 
 # filter through data frame based on specified meteorite class and select top n rows based on mass.
 def filter_data_by_class_and_top_n_heaviest(data, meteorite_class, top_n):
